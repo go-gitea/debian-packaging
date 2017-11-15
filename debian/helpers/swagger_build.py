c71584290e389494534f838c6ed360ee2a2da8d0
Michael Lustfield <michael@lustfield.net>
2017-11-15 01:02:51 -0600

    Script sync:
    - Output is now reproducible
    - Models section added
    - CSS cleaned up
    - Support for python 2 & 3

--- debian/helpers/swagger_build.py
+++ debian/helpers/swagger_build.py
@@ -14,7 +14,11 @@ import json
 import os
 import sys
 import textwrap
-import urllib2
+
+if sys.version_info.major >= 3:
+    from urllib.request import urlretrieve
+else:
+    from urllib import urlretrieve
 
 
 class Templates(object):
@@ -30,9 +34,6 @@ class Templates(object):
                 <h1>{title}</h1>
                 <h2>[ Version: {version} | Base URL: {baseurl} ]</h2>
                 <h3>{description}</h3>
-                <p>Produced by a hacky script to avoid Swagger-UI dependencies.
-                <br />Ref: <a href="https://wiki.debian.org/Javascript/Nodejs/Tasks/swagger-ui">
-                https://wiki.debian.org/Javascript/Nodejs/Tasks/swagger-ui</a></p>
                 {api_body}
                 {js}
               </div>
@@ -49,15 +50,30 @@ class Templates(object):
             </div>
             ''')
 
-    path = textwrap.dedent('''\
+    api_route = textwrap.dedent('''\
             <div class="opblock-section opblock-{api_method}">
               <a id="section-{section_id}" href="#" onclick="toggle_opbody(this.id); return false;">
                 <div class="opblock-summary">
                   <span class="opblock-summary-method summary-{api_method}">{api_method}</span>
                   <span class="opblock-summary-path">{api_path}</span>
+                  <span class="opblock-summary-description summary-model">{api_desc}</span>
                 </div>
               </a>
               <div class="opblock-body" style="display: none;" id="opbod-section-{section_id}">
+                {subsections}
+              </div>
+            </div>
+            ''')
+
+    api_model = textwrap.dedent('''\
+            <div class="opblock-section opblock-model">
+              <a id="model-{section_id}" href="#" onclick="toggle_opbody(this.id); return false;">
+                <div class="opblock-summary">
+                  <span class="opblock-summary-title summary-model">{api_title}</span>
+                  <span class="opblock-summary-description summary-model">{api_desc}</span>
+                </div>
+              </a>
+              <div class="opblock-body" style="display: none;" id="opbod-model-{section_id}">
                 {subsections}
               </div>
             </div>
@@ -91,7 +107,7 @@ class Templates(object):
 
     resp_row = textwrap.dedent('''\
             <tr>
-              <td class="pcol-name">{code}</td>
+              <td class="pcol-name">{name}</td>
               <td class="pcol-desc">{desc}</td>
             </tr>
             ''')
@@ -115,12 +131,14 @@ class Templates(object):
                         box-shadow: 0 0 3px rgba(0,0,0,.19); }
               .opblock-summary { display: flex; padding: 5px; align-items: center;
                         border-radius: 3px; text-shadow: 0 1px 0 rgba(0,0,0,.1); }
-              .opblock-summary-method { padding: 6px 15px; border-radius: 3px; text-align: center;
+              .opblock-summary-method { border-radius: 3px; text-align: center;
                         padding: 6px 15px; min-width: 80px; color: #ffffff; font-weight: 700; }
               .opblock-summary-path { font-size: 16px; display: flex; flex: 0 3 auto; word-break: break-all;
                         padding: 0 10px; font-family: monospace; font-weight: 600; }
-              .opblock-header { display: flex; align-items: center;  min-height: 50px;
+              .opblock-summary-description { font-size: 13px; }
+              .opblock-header { margin: 0 -20px; display: flex; align-items: center; min-height: 50px;
                         background: hsla(0,0%,100%,.8); box-shadow: 0 1px 2px rgba(0,0,0,.1); }
+              .opblock-header:first-child { margin-top: -20px; }
               .opblock-post { border-color: #49cc90; background: rgba(73,204,144,.1); }
               .opblock-delete { border-color: #f93e3e; background: rgba(249,62,62,.1); }
               .opblock-get { border-color: #61affe; background: rgba(97,175,254,.1); }
@@ -136,12 +154,21 @@ class Templates(object):
               .summary-delete { background: #f93e3e; }
               .summary-patch { background: #50e3c2; }
               .summary-put { background: #fca130; }
-              .opblock-table { padding: 20px 20px 10px; }
+              .opblock-body { padding: 20px; }
+              .opblock-table { padding: 20px 0; }
+              .opblock-table:last-child { padding-bottom: 0; }
+              #tag-section-Models > .opblock-model { background:rgba(0,0,0,.05); }
+              .opblock-model { border:1px solid rgba(59,65,81,.3); }
+              .opblock-summary-title { padding: 6px; font-weight: 700; }
+              .opblock-model .opblock-summary-description { padding: 0 10px 0 4px; }
+              .opblock-model .opblock-body { border-top:1px solid rgba(59,65,81,.3); }
               table { width: 100%; padding: 0 10px; border-collapse: collapse; }
               th { font-size: 12px; padding: 0 0 12px; text-align: left;
                         border-bottom: 1px solid rgba(59,65,81,.2); }
-              td { max-width: 20%; min-width: 6em; padding: 10px 0; vertical-align: top; }
+              td { width: 30%; padding: 10px 0; vertical-align: top; }
               .red { color: #ff0000; }
+              .blue { color: #5555aa; }
+              .grey { color: #999999; }
             </style>
             ''')
 
@@ -179,13 +206,13 @@ def main():
     write_static(page_data)
 
 
-def read_json(path='public/swagger.v1.json'):
+def read_json(path='swagger.v1.json'):
     '''Read JSON input from file.'''
     src = os.environ.get('SWAGGER_SRC', path)
     return json.load(open(src, 'r'))
 
 
-def write_static(page, destination='templates/swagger.tmpl'):
+def write_static(page, destination='swagger.html'):
     '''Generate a Swagger-like HTML page.
     Returns: True for write success | False for failure.'''
     dest = os.environ.get('SWAGGER_DST', destination)
@@ -199,13 +226,14 @@ def write_static(page, destination='templates/swagger.tmpl'):
 
 def render_page(json_data):
     '''Assemble the entire page.'''
+    for attr in ['description', 'title', 'license', 'version']:
+        if attr not in json_data['info']:
+            print('JSON data is missing required data: info:{}'.format(attr))
+            return False
     info = json_data['info']
     baseurl = json_data['basePath']
-    for attr in ['description', 'title', 'license', 'version']:
-        if attr not in info:
-            raise 'JSON data is missing required data: {}.'.format(attr)
-
-    api_body = build_body(json_data)
+    api_routes = build_routes(json_data)
+    api_models = build_models(json_data)
 
     return Templates.page.format(**{
         'js': Templates.static_js,
@@ -214,21 +242,103 @@ def render_page(json_data):
         'version': info['version'],
         'baseurl': baseurl,
         'description': info['description'],
-        'api_body': api_body})
+        'api_body': api_routes + api_models})
 
 
-def build_body(json_data):
+def build_models(json_data):
+    '''Build the HTML for a list of models.'''
+    if not json_data.get('definitions', False):
+        print('JSON data is missing required data: definitions')
+        return False
+
+    api_d = ''
+    model_id = 0
+    for model in sorted(json_data['definitions'].keys()):
+        model_id += 1
+        model_desc, subsections = build_model(json_data, model)
+        if model_desc.startswith(model):
+            model_desc = model_desc.replace(model, '', 1).strip().capitalize()
+        api_d += Templates.api_model.format(**{
+            'api_title': model,
+            'api_desc': model_desc,
+            'subsections': subsections,
+            'section_id': '{}'.format(model_id)})
+
+    return Templates.section.format(**{
+        'title': 'Models',
+        'api_d': api_d})
+
+
+def build_model(json_data, model):
+    '''Build the HTML for a model.'''
+    if not model in json_data.get('definitions', []):
+        print('Requested key "{}" not found in JSON data.'.format(model))
+        return False
+    mod = json_data['definitions'][model]
+    mod_desc = mod.get('description', '')
+
+    if 'properties' not in mod:
+        return mod_desc, '<table class="parameters"><tr><td>No properties</tr></td></table>'
+
+    rows = ''
+    for prop, attr in sorted(mod['properties'].items()):
+        if '$ref' in attr:
+            description = 'Model: {}'.format(attr['$ref'].split('/')[-1])
+        else:
+            fmt = ' (' + attr['format'] + ')' if attr.get('format', '') else ''
+            description = '{}<br />{}<br />{}'.format(
+                '<span class="blue">{}{}</span>'.format(attr.get('type', 'undef'), fmt),
+                '<span class="grey">x-go-name: {}</span>'.format(attr.get('x-go-name', 'undef')),
+                attr.get('description', ''))
+
+        rows += Templates.resp_row.format(**{
+            'name': prop,
+            'desc': description})
+
+    return mod_desc, Templates.table.format(**{
+        'headers': '',
+        'table_rows': rows})
+
+
+def build_routes(json_data):
     '''Build the API docs portion of the page.'''
+    if not json_data.get('paths', ''):
+        print('JSON data is missing required data: paths')
+        return False
     paths = json_data['paths']
     topics = gen_topics(paths)
+
     sb = ''
     section_id = 0
-    for k, v in sorted(topics.iteritems()):
+    for tag, routes in sorted(topics.items()):
         section_id += 1
-        api_d = build_section(json_data, v, section_id)
+        api_d = build_route(json_data, routes, section_id)
         sb += Templates.section.format(**{
-            'title': k,
+            'title': tag,
             'api_d': api_d})
+    return sb
+
+def build_route(json_data, keys, section_id=0):
+    '''Build the HTML for a list (keys) of available API queries.'''
+    paths = json_data['paths']
+
+    # Attempt to sort by group, then length, then path+method
+    keys.sort(key=lambda k: k[0:3] + str(len(k.split('^^')[0])) + k)
+
+    sb = ''
+    key_id = 0
+    for key in keys:
+        key_id += 1
+        path, method = key.split('^^')
+        desc = paths[path][method].get('summary', '')
+        parameters = build_parameter_table(paths[path][method])
+        responses = build_responses_table(paths[path][method], json_data)
+        sb += Templates.api_route.format(**{
+            'api_path': path,
+            'api_desc': desc,
+            'api_method': method,
+            'subsections': str(parameters + responses),
+            'section_id': '{}-{}'.format(section_id, key_id)})
     return sb
 
 
@@ -248,31 +358,13 @@ def gen_topics(paths):
     return tags
 
 
-def build_section(json_data, keys, section_id=0):
-    '''Build a HTML for a list (keys) of available API queries.'''
-    paths = json_data['paths']
-    sb = ''
-    key_id = 0
-    for key in sorted(keys):
-        key_id += 1
-        path, method = key.split('^^')
-        parameters = build_parameter_table(paths[path][method])
-        responses = build_responses_table(paths[path][method], json_data)
-        sb += Templates.path.format(**{
-            'api_path': path,
-            'api_method': method,
-            'subsections': str(parameters + responses),
-            'section_id': '{}-{}'.format(section_id, key_id)})
-    return sb
-
-
 def build_parameter_table(api):
     '''Build and return a table of parameters for an API method.'''
     if 'parameters' not in api:
         return '<table class="parameters"><tr><td>No parameters</tr></td></table>'
 
     rows = ''
-    for row in api['parameters']:
+    for row in sorted(api['parameters'], key=lambda k: k['name']):
         description = row.get('description', '')
         required = ' <span class="red">*</span>' if row.get('required', False) else ''
         if 'type' in row:
@@ -286,7 +378,7 @@ def build_parameter_table(api):
             'desc': description,
             'type': vartype})
 
-    table =  Templates.table.format(**{
+    table = Templates.table.format(**{
         'headers': '<th>Name</th><th>Type</th><th>Description</th>',
         'table_rows': rows})
 
@@ -301,10 +393,10 @@ def build_responses_table(api, json_data):
         return ''
 
     rows = ''
-    for code, row in api['responses'].items():
+    for code, row in sorted(api['responses'].items()):
         description = find_response(row['$ref'], json_data) if '$ref' in row else ''
         rows += Templates.resp_row.format(**{
-            'code': code,
+            'name': code,
             'desc': description})
 
     table = Templates.table.format(**{
@@ -331,10 +423,7 @@ def download_json(url='https://try.gitea.io/swagger.v1.json', output='swagger.v1
     out = os.environ.get('SWAGGER_DST', output)
     src = os.environ.get('SWAGGER_URL', url)
     if not os.path.exists(out):
-        response = urllib2.urlopen(src)
-        json_data = response.read()
-        with open(out, 'w') as fh:
-            fh.write(json_data)
+        urlretrieve(src, out)
 
 
 if __name__ == '__main__':
